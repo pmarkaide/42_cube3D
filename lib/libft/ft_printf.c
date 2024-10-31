ae50009748405852a21c256d0f6701ef5fb0c0a2
Pablo Markaide <pablo.markaide@gmail.com>
2024-10-31 08:42:16 +0200

    feat: ft_printf now prints to any given file descriptor

--- lib/libft/ft_printf.c
+++ lib/libft/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 14:22:12 by pmarkaid          #+#    #+#             */
-/*   Updated: 2024/01/17 14:47:21 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2024/10/31 08:40:29 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,33 +20,33 @@ int	ft_error_w(int print_count, int write_return)
 		return (print_count + write_return);
 }
 
-int	ft_putformat(char specifier, va_list args)
+int	ft_putformat(int fd, char specifier, va_list args)
 {
 	int	p;
 
 	p = 0;
 	if (specifier == '%')
-		p = ft_error_w(p, ft_putchar_fd('%', 1));
+		p = ft_error_w(p, ft_putchar_fd('%', fd));
 	else if (specifier == 'c')
-		p = ft_error_w(p, ft_putchar_fd(va_arg(args, int), 1));
+		p = ft_error_w(p, ft_putchar_fd(va_arg(args, int), fd));
 	else if (specifier == 's')
-		p = ft_error_w(p, ft_putstr_fd(va_arg(args, char *), 1));
+		p = ft_error_w(p, ft_putstr_fd(va_arg(args, char *), fd));
 	else if (specifier == 'd' || specifier == 'i')
-		p = ft_error_w(p, ft_putnbr_fd(va_arg(args, int), 1));
+		p = ft_error_w(p, ft_putnbr_fd(va_arg(args, int),fd));
 	else if (specifier == 'u')
-		p = ft_error_w(p, ft_putnbr_fd_uns(va_arg(args, unsigned long), 1));
+		p = ft_error_w(p, ft_putnbr_fd_uns(va_arg(args, unsigned long), fd));
 	else if (specifier == 'x')
-		p = ft_error_w(p, ft_putnbr_fd_hex(va_arg(args, unsigned int), 1, 0));
+		p = ft_error_w(p, ft_putnbr_fd_hex(va_arg(args, unsigned int), fd, 0));
 	else if (specifier == 'X')
-		p = ft_error_w(p, ft_putnbr_fd_hex(va_arg(args, unsigned int), 1, 1));
+		p = ft_error_w(p, ft_putnbr_fd_hex(va_arg(args, unsigned int), fd, 1));
 	else if (specifier == 'p')
-		p = ft_error_w(p, ft_putptr_fd(va_arg(args, unsigned long), 1));
+		p = ft_error_w(p, ft_putptr_fd(va_arg(args, unsigned long), fd));
 	if (p == -1)
 		return (-1);
 	return (p);
 }
 
-int	ft_printf(const char *format, ...)
+int	ft_printf(int fd, const char *format, ...)
 {
 	va_list	args;
 	int		p;
@@ -57,13 +57,13 @@ int	ft_printf(const char *format, ...)
 	{
 		if (*format == '%')
 		{
-			p = ft_error_w(p, ft_putformat(*(++format), args));
+			p = ft_error_w(p, ft_putformat(fd, *(++format), args));
 			if (p == -1)
 				return (-1);
 		}
 		else
 		{
-			p = ft_error_w(p, write(1, format, 1));
+			p = ft_error_w(p, write(fd, format, 1));
 			if (p == -1)
 				return (-1);
 		}
