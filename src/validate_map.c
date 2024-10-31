3a060d6c765c0c3f64dbfc87d166815c2bb868b4
Pablo Markaide <pablo.markaide@gmail.com>
2024-10-31 09:50:53 +0200

    sync work

--- src/validate_map.c
+++ src/validate_map.c
@@ -6,7 +6,7 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 15:05:20 by pmarkaid          #+#    #+#             */
-/*   Updated: 2024/10/31 08:43:22 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2024/10/31 09:23:46 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,12 +65,60 @@ static int	eval_file(char *file, char *ext)
 	return(0);
 }
 
+
+int parse_line(char *line, t_macro *macro)
+{
+	if(ft_strncmp(line, "\n",1))
+		return(0);
+	else if(ft_strncmp(line, "NO",2) == 0)
+		macro->map->no = ft_strdup(ft_skipws(line + 2));
+	else if(ft_strncmp(line, "SO",2) == 0)
+		macro->map->so = ft_strdup(ft_skipws(line + 2));
+	else if(ft_strncmp(line, "WE",2) == 0)
+		macro->map->we = ft_strdup(ft_skipws(line + 2));
+	else if(ft_strncmp(line, "EA",2) == 0)
+		macro->map->ea = ft_strdup(ft_skipws(line + 2));
+	else if(ft_strncmp(line, "F",1) == 0)
+	{
+		macro->map->f[0] = ft_atoi(ft_skipws(line + 1));
+		macro->map->f[1] = ft_atoi(ft_skipws(line + 3));
+		macro->map->f[2] = ft_atoi(ft_skipws(line + 5));
+	}
+	else if(ft_strncmp(line, "C",1) == 0)
+	{
+		macro->map->c[0] = ft_atoi(ft_skipws(line + 1));
+		macro->map->c[1] = ft_atoi(ft_skipws(line + 3));
+		macro->map->c[2] = ft_atoi(ft_skipws(line + 5));
+	}
+}
+
+int read_file(char *file, t_macro *macro)
+{
+	char *line;
+	int fd;
+	
+	fd = open(file, O_RDONLY);
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		line = ft_skipws(line);
+		if (parse_line(line, macro))
+		{
+			free(line);
+			close(fd);
+			return (1);
+		}
+		free(line);
+	}
+	close(fd);
+	return (0);
+}
+
 void	validate_map(char *file, t_macro *macro)
 {
 	if (eval_file(file, ".cub"))
 		free_and_exit(macro);
-	// if (read_file(file, macro))
-	// 	free_and_exit(macro);
+	if (read_file(file, macro))
+		free_and_exit(macro);
 	// if (validate_contents(macro))
 	// 	free_and_exit(macro);
 }
