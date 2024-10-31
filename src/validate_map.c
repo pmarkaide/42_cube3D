80a7dd2342acee87d7d7fff8fab939594934e960
Pablo Markaide <pablo.markaide@gmail.com>
2024-10-31 14:12:17 +0200

    feat: add section detection function and texture parse MVP

--- src/validate_map.c
+++ src/validate_map.c
@@ -6,7 +6,7 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 15:05:20 by pmarkaid          #+#    #+#             */
-/*   Updated: 2024/10/31 09:23:46 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2024/10/31 14:11:42 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ static int	eval_file(char *file, char *ext)
 	char	*dot;
 	int fd;
 
-	if(ext == ".cub")
+	if(ft_strcmp(ext,".cub") == 0)
 	{
 		dot = ft_strrchr(file, '.');
 		if(dot == NULL || (dot != NULL && ft_strcmp(dot, ".cub")))
@@ -66,11 +66,9 @@ static int	eval_file(char *file, char *ext)
 }
 
 
-int parse_line(char *line, t_macro *macro)
+int parse_textures(char *line, t_macro *macro)
 {
-	if(ft_strncmp(line, "\n",1))
-		return(0);
-	else if(ft_strncmp(line, "NO",2) == 0)
+	if(ft_strncmp(line, "NO",2) == 0)
 		macro->map->no = ft_strdup(ft_skipws(line + 2));
 	else if(ft_strncmp(line, "SO",2) == 0)
 		macro->map->so = ft_strdup(ft_skipws(line + 2));
@@ -78,30 +76,69 @@ int parse_line(char *line, t_macro *macro)
 		macro->map->we = ft_strdup(ft_skipws(line + 2));
 	else if(ft_strncmp(line, "EA",2) == 0)
 		macro->map->ea = ft_strdup(ft_skipws(line + 2));
-	else if(ft_strncmp(line, "F",1) == 0)
-	{
-		macro->map->f[0] = ft_atoi(ft_skipws(line + 1));
-		macro->map->f[1] = ft_atoi(ft_skipws(line + 3));
-		macro->map->f[2] = ft_atoi(ft_skipws(line + 5));
-	}
-	else if(ft_strncmp(line, "C",1) == 0)
-	{
-		macro->map->c[0] = ft_atoi(ft_skipws(line + 1));
-		macro->map->c[1] = ft_atoi(ft_skipws(line + 3));
-		macro->map->c[2] = ft_atoi(ft_skipws(line + 5));
-	}
+	return(0);
+}
+
+// int parse_colors(char *line, t_macro *macro)
+// {
+// 	else if(ft_strncmp(line, "F",1) == 0)
+// 	{
+// 		macro->map->f[0] = ft_atoi(ft_skipws(line + 1));
+// 		macro->map->f[1] = ft_atoi(ft_skipws(line + 3));
+// 		macro->map->f[2] = ft_atoi(ft_skipws(line + 5));
+// 	}
+// 	else if(ft_strncmp(line, "C",1) == 0)
+// 	{
+// 		macro->map->c[0] = ft_atoi(ft_skipws(line + 1));
+// 		macro->map->c[1] = ft_atoi(ft_skipws(line + 3));
+// 		macro->map->c[2] = ft_atoi(ft_skipws(line + 5));
+// 	}
+// }
+
+int detect_section(char *line)
+{
+	char *skipped;
+	int section;
+
+	skipped = ft_skipws(line);
+	if(ft_strncmp(skipped, "NO",2) == 0)
+		section= 1;
+	else if(ft_strncmp(skipped, "SO",2) == 0)
+		section= 1;
+	else if(ft_strncmp(skipped, "WE",2) == 0)
+		section= 1;
+	else if(ft_strncmp(skipped, "EA",2) == 0)
+		section= 1;
+	else if(ft_strncmp(skipped, "F",1) == 0)
+		section= 2;
+	else if(ft_strncmp(skipped, "C",1) == 0)
+		section= 2;
+	else
+		section= 3;
+	return(section);
+}
+
+int parse_line(char *line, t_macro *macro, int section)
+{
+	int err;
+
+	err = 0;
+	if(section == 1)
+		err = parse_textures(line, macro);
+	return(err);
 }
 
 int read_file(char *file, t_macro *macro)
 {
 	char *line;
 	int fd;
+	int section;
 	
 	fd = open(file, O_RDONLY);
 	while ((line = get_next_line(fd)) != NULL)
 	{
-		line = ft_skipws(line);
-		if (parse_line(line, macro))
+		section = detect_section(line);
+		if (parse_line(line, macro, section))
 		{
 			free(line);
 			close(fd);
@@ -119,6 +156,7 @@ void	validate_map(char *file, t_macro *macro)
 		free_and_exit(macro);
 	if (read_file(file, macro))
 		free_and_exit(macro);
+	print_map_struct(macro->map);
 	// if (validate_contents(macro))
 	// 	free_and_exit(macro);
 }
